3314cadeef73d9b0a787a1ffa26d979c7b333ab0
Kennan Zhong <kenzhong@gmail.com>
2018-01-06 16:01:48 -0500

    add images to assets folder and update projects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,6 +4,42 @@ const Projects = (props) => (
   <div className='modal'>
     <h2>Recent Projects</h2>
     <span className='btn-close-modal' onClick={props.closeModal}>✖</span>
+
+    <main>
+      <div className='project-item'>Pxl-On
+        <div className='project-img'> image here </div>
+        <div className='project-description'>
+          <p>A full stack photo sharing app inspired by 500px and Instagram, built using
+            React/Redux on the frontend and a Ruby on Rails RESTful API.
+            <a href='https://pxl-on.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Live</a> |
+            <a href='https://github.com/ken-zhong/pxl-On' rel='noopener noreferrer' target='_blank'>Repo</a>
+          </p>
+        </div>
+      </div>
+
+      <div className='project-item'>Plasmaforce
+        <div className='project-img'> image here </div>
+        <div className='project-description'>
+          <p>A sidescrolling 2d space shooter built using JavaScript, HTML5, and lots of
+            object-oriented programming.
+            <a href='https://ken-zhong.github.io/plasmaforce/' rel='noopener noreferrer' target='_blank'>Live</a> |
+            <a href='https://github.com/ken-zhong/plasmaforce' rel='noopener noreferrer' target='_blank'>Repo</a>
+          </p>
+        </div>
+      </div>
+
+      <div className='project-item'>Pictus Whiteboard
+        <div className='project-img'> image here </div>
+        <div className='project-description'>
+          <p>A whiteboarding app that connects users together with WebSockets and allows them to
+            collaborate and draw together on a whiteboard. Built with Node/Express, MongoDB, and
+            Socket.io.
+            <a href='https://pictus-whiteboard.herokuapp.com/' rel='noopener noreferrer' target='_blank'>Live</a> |
+            <a href='https://github.com/ken-zhong/pictus' rel='noopener noreferrer' target='_blank'>Repo</a>
+          </p>
+        </div>
+      </div>
+    </main>
   </div>
 );
 
--- src/components/skills.js
+++ src/components/skills.js
@@ -13,30 +13,30 @@ const Experience = (props) => (
       </p>
 
       <div className='kz-devicon-container'>
-        <i className='devicon-javascript-plain colored'></i>
-        <i className='devicon-react-original-wordmark colored'></i>
+        <i className='devicon-javascript-plain colored' />
+        <i className='devicon-react-original-wordmark colored' />
         <span>
           <img src={reduxLogo} alt='' />
         </span>
-        <i className='devicon-ruby-plain-wordmark colored'></i>
-        <i className='devicon-rails-plain-wordmark colored'></i>
-        <i className='devicon-nodejs-plain-wordmark colored'></i>
-        <i className='devicon-express-original-wordmark'></i>
-        <i className='devicon-postgresql-plain-wordmark colored'></i>
-        <i className='devicon-mongodb-plain-wordmark colored'></i>
+        <i className='devicon-ruby-plain-wordmark colored' />
+        <i className='devicon-rails-plain-wordmark colored' />
+        <i className='devicon-nodejs-plain-wordmark colored' />
+        <i className='devicon-express-original-wordmark' />
+        <i className='devicon-postgresql-plain-wordmark colored' />
+        <i className='devicon-mongodb-plain-wordmark colored' />
 
-        <i className='devicon-webpack-plain-wordmark colored'></i>
-        <i className='devicon-babel-plain colored'></i>
-        <i className='devicon-heroku-plain-wordmark colored'></i>
-        <i className='devicon-amazonwebservices-plain-wordmark colored'></i>
+        <i className='devicon-webpack-plain-wordmark colored' />
+        <i className='devicon-babel-plain colored' />
+        <i className='devicon-heroku-plain-wordmark colored' />
+        <i className='devicon-amazonwebservices-plain-wordmark colored' />
 
-        <i className='devicon-atom-original-wordmark'></i>
-        <i className='devicon-jquery-plain-wordmark colored'></i>
-        <i className='devicon-html5-plain-wordmark colored'></i>
-        <i className='devicon-css3-plain-wordmark colored'></i>
-        <i className='devicon-bootstrap-plain-wordmark colored'></i>
-        <i className='devicon-git-plain-wordmark colored'></i>
-        <i className='devicon-github-plain-wordmark'></i>
+        <i className='devicon-atom-original-wordmark' />
+        <i className='devicon-jquery-plain-wordmark colored' />
+        <i className='devicon-html5-plain-wordmark colored' />
+        <i className='devicon-css3-plain-wordmark colored' />
+        <i className='devicon-bootstrap-plain-wordmark colored' />
+        <i className='devicon-git-plain-wordmark colored' />
+        <i className='devicon-github-plain-wordmark' />
       </div>
     </main>
 
