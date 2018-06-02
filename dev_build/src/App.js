5acac5e056f16fb80bf70f2767c74c8e193eb364
Ken Zhong <kenzhong@gmail.com>
2018-06-02 10:52:31 -0400

    update resume, add click outside detection

--- dev_build/src/App.js
+++ dev_build/src/App.js
@@ -2,52 +2,76 @@ import React, { Component } from 'react';
 import Experience from './components/experience';
 import Projects from './components/projects';
 import Skills from './components/skills';
-import Resume from './assets/kzhong-resume.pdf'
+import Resume from './assets/Ken_Zhong_resume_2018.pdf';
 import './App.css';
 import './Modal.css';
 
 class App extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
       showModal: false
     };
+    this.closeModal = this.closeModal.bind(this);
   }
 
-  openModal (modal) {
-    this.setState({showModal: modal});
+  componentWillMount() {
+    document.addEventListener('mousedown', this.handleClick, false);
   }
 
-  closeModal () {
-    this.setState({showModal: false});
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClick, false);
   }
 
-  renderMain () {
-    // <a href={Resume}>RESUME</a>
+  handleClick = e => {
+    console.log(e.target);
+    if (this.state.showModal && e.target.className === 'App') this.closeModal();
+  };
+
+  openModal(modal) {
+    this.setState({ showModal: modal });
+  }
+
+  closeModal() {
+    this.setState({ showModal: false });
+  }
+
+  renderMain() {
     return (
-      <div className='main-content'>
+      <div className="main-content">
         <h1>Hi, I'm Ken Zhong</h1>
         <h4>
-          <a href='https://github.com/ken-zhong' target='_blank' rel='noopener noreferrer'>GITHUB</a>
+          <a href="https://github.com/ken-zhong" target="_blank" rel="noopener noreferrer">
+            GITHUB
+          </a>
           &nbsp;&nbsp;·&nbsp;&nbsp;
-          <a href="#">RESUME</a>
+          <a href={Resume}>RESUME</a>
           &nbsp;&nbsp;·&nbsp;&nbsp;
-          <a href='mailto:kenzhong@gmail.com'>EMAIL ME</a>
+          <a href="mailto:kenzhong@gmail.com">EMAIL ME</a>
         </h4>
 
         <p>
-          I'm a NYC-based full stack software engineer who's passionate about
-          learning new things and creating experiences. I'm currently working with
-          React/Redux and Ruby on Rails.
+          I'm a NYC-based full stack software engineer who's passionate about learning new things
+          and creating experiences. I'm currently working with React, GraphQL, and Ruby on Rails.
         </p>
-        <p>
-          When I'm not coding, I enjoy reading, photography, and playing tennis.
-        </p>
+        <p>When I'm not coding, I enjoy reading, photography, and playing tennis.</p>
 
         <ul>
-          <li><span className='button-std' onClick={() => this.openModal('experience')}>Experience</span></li>
-          <li><span className='button-std' onClick={() => this.openModal('projects')}>Recent Projects</span></li>
-          <li><span className='button-std' onClick={() => this.openModal('skills')}>Skills</span></li>
+          <li>
+            <span className="button-std" onClick={() => this.openModal('experience')}>
+              Experience
+            </span>
+          </li>
+          <li>
+            <span className="button-std" onClick={() => this.openModal('projects')}>
+              Recent Projects
+            </span>
+          </li>
+          <li>
+            <span className="button-std" onClick={() => this.openModal('skills')}>
+              Skills
+            </span>
+          </li>
         </ul>
 
         <footer>© Ken Zhong {new Date().getFullYear()}</footer>
@@ -55,20 +79,22 @@ class App extends Component {
     );
   }
 
-  renderModal () {
+  renderModal() {
     switch (this.state.showModal) {
-      case 'skills': return <Skills closeModal={this.closeModal.bind(this)} />;
-      case 'experience': return <Experience closeModal={this.closeModal.bind(this)} />;
-      case 'projects': return <Projects closeModal={this.closeModal.bind(this)} />;
-      default: return 'ERROR';
+      case 'skills':
+        return <Skills closeModal={this.closeModal} />;
+      case 'experience':
+        return <Experience closeModal={this.closeModal} />;
+      case 'projects':
+        return <Projects closeModal={this.closeModal} />;
+      default:
+        return 'ERROR';
     }
   }
 
-  render () {
+  render() {
     return (
-      <div className='App'>
-        { this.state.showModal ? this.renderModal() : this.renderMain() }
-      </div>
+      <div className="App">{this.state.showModal ? this.renderModal() : this.renderMain()}</div>
     );
   }
 }
--- dev_build/src/components/experience.js
+++ dev_build/src/components/experience.js
@@ -1,56 +1,75 @@
 import React from 'react';
 
-const Experience = (props) => (
-  <div className='modal'>
-    <span className='btn-close-modal' onClick={props.closeModal}>✖</span>
+const Experience = props => (
+  <div className="modal">
+    <span className="btn-close-modal" onClick={props.closeModal}>
+      ✖
+    </span>
     <h2>Software Experience</h2>
-
     <main>
-      <div className='experience-blurb'>
-        <span className='experience-company'>SumZero</span>
-        <div className='experience-header'>
+      <div className="experience-blurb">
+        <span className="experience-company">SumZero</span>
+        <div className="experience-header">
           <span>Software Engineer</span>
           <span>1/2018 - present</span>
         </div>
-        <ul className='experience-description'>
-          <li>Developed features for a full stack application utilizing Ruby on Rails, JavaScript, React, MySQL, and GraphQL.</li>
-          <li>Wrote backend unit tests with RSPEC and frontend tests with Jest, Enzyme, and Storybook, improving test coverage and application reliability. </li>
+        <ul className="experience-description">
+          <li>
+            Developed various features for SumZero as a full stack software engineer utilizing Ruby
+            on Rails, JavaScript, React, MySQL, and GraphQL.
+          </li>
+          <li>
+            Overhauled main search functionality with a multi-index ElasticSearch model,
+            significantly improving both querying speed and result accuracy.
+          </li>
+          <li>
+            Wrote backend unit tests with RSPEC and Capybara and frontend tests with Jest, Enzyme,
+            and Storybook, improving test coverage and application reliability.
+          </li>
+          <li>
+            Converted Angular 1.x with RESTful AJAX components into React/Redux components with
+            GraphQL queries and mutations, improving maintainability and ease of deployment of
+            future features.
+          </li>
         </ul>
       </div>
     </main>
 
+    <h2>Other Work Experience</h2>
+    <main>
+      <div className="experience-blurb">
+        <span className="experience-company">Testmasters</span>
+        <div className="experience-header">
+          <span>Instructor</span>
+          <span>2/2016 - 9/2017</span>
+        </div>
+        <ul className="experience-description">
+          <li>
+            Taught live in-person classes for students preparing for the LSAT exam, as well as
+            provided private one-on-one tutoring.
+          </li>
+        </ul>
+      </div>
+      <div className="experience-blurb">
+        <span className="experience-company">Microsoft</span>
+        <div className="experience-header">
+          <span>Public Policy Fellow</span>
+          <span>9/2013 - 9/2014</span>
+        </div>
+        <ul className="experience-description">
+          <li>
+            Researched and drafted memoranda on regulatory and policy issues in privacy,
+            cyber-security, government surveillance, and telecommunications in support of
+            Microsoft’s policy outreach.
+          </li>
+          <li>
+            Created and maintained Microsoft’s internal legislative tracker to monitor and analyze
+            Congressional legislation on government surveillance programs and privacy reform.
+          </li>
+        </ul>
+      </div>
+    </main>
   </div>
 );
 
 export default Experience;
-
-// <li>Rebuilt site's main omnisearch index using Elasticsearch, improving query times and accuracy.</li>
-
-//     <h2>Previous Experience</h2>
-//     <main>
-//       <div className='experience-blurb'>
-//         <span className='experience-company'>Testmasters</span>
-//         <div className='experience-header'>
-//           <span>Instructor</span>
-//           <span>2/2016 - 9/2017</span>
-//         </div>
-//         <ul className='experience-description'>
-//           <li>Taught live in-person classes for students preparing for the LSAT exam, as well as provided
-//             private one-on-one tutoring.</li>
-//         </ul>
-//       </div>
-//
-//       <div className='experience-blurb'>
-//         <span className='experience-company'>Microsoft</span>
-//         <div className='experience-header'>
-//           <span>Public Policy Fellow</span>
-//           <span>9/2013 - 9/2014</span>
-//         </div>
-//         <ul className='experience-description'>
-//           <li>Researched and drafted memoranda on regulatory and policy issues in privacy, cyber-security,
-// government surveillance, and telecommunications in support of Microsoft’s policy outreach.</li>
-//           <li>Created and maintained Microsoft’s internal legislative tracker to monitor and analyze
-// Congressional legislation on government surveillance programs and privacy reform.</li>
-//         </ul>
-//       </div>
-//     </main>
