109d72f4d16212e2b6b4d09bc877b0a4d5c9f416
Kennan Zhong <kenzhong@gmail.com>
2020-02-21 21:28:12 -0500

    add path times project

--- dev_build/src/components/projects.js
+++ dev_build/src/components/projects.js
@@ -2,52 +2,117 @@ import React from 'react';
 import PxlImg from '../assets/pxl-on.jpg';
 import PlasmaforceImg from '../assets/plasmaforce.gif';
 import PictusImg from '../assets/pictus.jpg';
+import PathTimesImg from '../assets/pathtimes.png';
 
-const Projects = (props) => (
-  <div className='modal'>
+const Projects = props => (
+  <div className="modal">
     <h2>Recent Projects</h2>
-    <span className='btn-close-modal' onClick={props.closeModal}>✖</span>
+    <span className="btn-close-modal" onClick={props.closeModal}>
+      ✖
+    </span>
 
     <main>
-      <div className='project-item'>
-        <div className='project-img'> <img src={PxlImg} /> </div>
-        <h3><a href='https://pxl-on.herokuapp.com/#/'>Pxl-On</a></h3>
-        <div className='project-description'>
-          <p>A full stack photo sharing app inspired by 500px and Instagram, built using
-            React/Redux on the frontend and a Ruby on Rails RESTful API.&nbsp;
-            <a href='https://pxl-on.herokuapp.com/' rel='noopener noreferrer'
-              target='_blank'>Live</a> |&nbsp;
-            <a href='https://github.com/ken-zhong/pxl-On'
-              rel='noopener noreferrer' target='_blank'>Repo</a>
+      <div className="project-item">
+        <div className="project-img">
+          <img src={PathTimesImg} />
+        </div>
+        <h3>
+          <a href="https://kenzhong.me/path-times/">Path Times</a>
+        </h3>
+        <div className="project-description">
+          <p>
+            A simple PWA app to get NJ Path Train times built using TypeScript, React, and
+            Material-UI.{' '}
+            <a href="https://kenzhong.me/path-times/" rel="noopener noreferrer" target="_blank">
+              Live
+            </a>{' '}
+            |{' '}
+            <a
+              href="https://github.com/ken-zhong/path-times"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Repo
+            </a>
           </p>
         </div>
       </div>
 
-      <div className='project-item'>
-        <div className='project-img'> <img src={PlasmaforceImg} /> </div>
-        <h3><a href='https://kenzhong.me/plasmaforce/' >Plasmaforce</a></h3>
-        <div className='project-description'>
-          <p>A sidescrolling 2d space shooter built using JavaScript, HTML5, and lots of
-            object-oriented programming.&nbsp;
-            <a href='https://ken-zhong.github.io/plasmaforce/'
-              rel='noopener noreferrer' target='_blank'>Live</a> |&nbsp;
-            <a href='https://github.com/ken-zhong/plasmaforce'
-              rel='noopener noreferrer' target='_blank'>Repo</a>
+      <div className="project-item">
+        <div className="project-img">
+          <img src={PxlImg} />
+        </div>
+        <h3>
+          <a href="https://pxl-on.herokuapp.com/#/">Pxl-On</a>
+        </h3>
+        <div className="project-description">
+          <p>
+            A full stack photo sharing app inspired by 500px and Instagram, built using React/Redux
+            on the frontend and a Ruby on Rails RESTful API.{' '}
+            <a href="https://pxl-on.herokuapp.com/" rel="noopener noreferrer" target="_blank">
+              Live
+            </a>{' '}
+            |{' '}
+            <a href="https://github.com/ken-zhong/pxl-On" rel="noopener noreferrer" target="_blank">
+              Repo
+            </a>
           </p>
         </div>
       </div>
 
-      <div className='project-item'>
-        <div className='project-img'> <img src={PictusImg} /> </div>
-        <h3><a href='https://pictus-whiteboard.herokuapp.com/'>Pictus Whiteboard</a></h3>
-        <div className='project-description'>
-          <p>A whiteboarding app that connects users together with WebSockets and allows them to
+      <div className="project-item">
+        <div className="project-img">
+          <img src={PlasmaforceImg} />
+        </div>
+        <h3>
+          <a href="https://kenzhong.me/plasmaforce/">Plasmaforce</a>
+        </h3>
+        <div className="project-description">
+          <p>
+            A sidescrolling 2d space shooter built using JavaScript, HTML5, and lots of
+            object-oriented programming.{' '}
+            <a
+              href="https://ken-zhong.github.io/plasmaforce/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Live
+            </a>{' '}
+            |{' '}
+            <a
+              href="https://github.com/ken-zhong/plasmaforce"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Repo
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <div className="project-item">
+        <div className="project-img">
+          <img src={PictusImg} />
+        </div>
+        <h3>
+          <a href="https://pictus-whiteboard.herokuapp.com/">Pictus Whiteboard</a>
+        </h3>
+        <div className="project-description">
+          <p>
+            A whiteboarding app that connects users together with WebSockets and allows them to
             collaborate and draw together on a whiteboard. Built with Node/Express, MongoDB, and
-            Socket.io.&nbsp;
-            <a href='https://pictus-whiteboard.herokuapp.com/'
-              rel='noopener noreferrer' target='_blank'>Live</a> |&nbsp;
-            <a href='https://github.com/ken-zhong/pictus'
-              rel='noopener noreferrer' target='_blank'>Repo</a>
+            Socket.io.{' '}
+            <a
+              href="https://pictus-whiteboard.herokuapp.com/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Live
+            </a>{' '}
+            |{' '}
+            <a href="https://github.com/ken-zhong/pictus" rel="noopener noreferrer" target="_blank">
+              Repo
+            </a>
           </p>
         </div>
       </div>
